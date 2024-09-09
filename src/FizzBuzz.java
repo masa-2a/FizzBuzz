d7971f3f5194f6c69af950ba3be8c3a5c0d5c590
Masa Arja <masaarja@Masas-MacBook-Pro.local>
2024-09-09 09:52:31 -0400

    changed Fizzbuzz to while loop

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,42 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+//setting up class FizzBuzz
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i < 100; i++) {
+        //create index
+        int  i = 1;
+        //while loop that starts at 1 and increments by 1 until 100
+        while (i <= 100) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+            i = getI(i);
         }
+    }
+
+    private static int getI(int i) {
+        boolean divisibleBy3 = i % 3 == 0; //when initialsing a variable you add its type [name] = ...
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) { // add curly brackets at the end of any if/for/while
+            i += 1;
+            System.out.println("Fizz Buzz"); // you usually shouldn't print stuff out
+
+        } else if (divisibleBy3) {
+            i += 1;
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+            i += 1;
+            System.out.println("Buzz");
+
+        } else {
+            i += 1;
+            System.out.println(i);
+
+        }
+        return i;
     }
 }
